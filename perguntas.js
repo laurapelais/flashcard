450ff8b6ef5694ac0412863041c3a4cd63bd0d40
Laura Firbida Pelais <138526482+laurapelais@users.noreply.github.com>
2024-10-01 08:14:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quanto tempo a Terra demora para dar uma volta completa em torno dela mesma?',
+    'Aproximadamente 24 horas'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quantos ossos temos no nosso corpo?',
+    '206'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quais são as fases da Lua?',
+    'Nova, crescente, cheia e minguante'
+)
